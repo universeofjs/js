dd64f9833c13cc1970f88c2e56b4e41bffe482c4
Ankit Jain <t443296@10.37.32.35>
2017-01-08 03:28:00 -0500

    added Header component

---
+++ reactWidget/src/client/app/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default class Header extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1><img src="/img/deluxe.png" alt="Deluxe Rewards"/></h1>
+				<hr/>
+            </div>
+        );
+    }
+};
