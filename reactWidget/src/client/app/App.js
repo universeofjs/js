6d5fa5356ea2f6cf617b81d10a9f5e135ca94f67
Ankit Jain <t443296@10.37.32.35>
2017-02-19 19:33:23 -0500

    cleaned up App.js

--- reactWidget/src/client/app/App.js
+++ reactWidget/src/client/app/App.js
@@ -1,138 +1,19 @@
 
-import React, {PropTypes} from 'react';
-import {render} from 'react-dom';
-
-import AwesomeComponent from './AwesomeComponent.js';
-
-import AddBirthdayComponent from './BirthdayComponent.js';
-import ReleaseComponent from './ReleaseComponent.js';
-import ProgressForm from './progressForm/ProgressForm.js';
-
-import ShowEmployees from './ShowEmployees.js';
-import BirthdayCard from './BirthdayCard.js';
-import ShowEmployeesCards from './ShowEmployeesCards.js';
-
-import ShowReleases from './ShowReleases.js';
-import ShowReleaseCards from './ShowReleaseCards.js';
-
-import ShowTeams from './ShowTeams.js';
-
+import React from 'react';
 import Header from './Header';
 import DeluxeCoreValues from './DeluxeCoreValues';
 import EmployeesOfTheMonth from './EmployeesOfTheMonth';
 import QuoteOfTheDay from './QuoteOfTheDay';
 import UpcomingEvents from './UpcomingEvents';
+import ShowTeams from './ShowTeams';
+import AwesomeComponent from './AwesomeComponent';
+import BirthdayCard from './BirthdayCard';
+import ShowReleaseCards from './ShowReleaseCards';
 import Footer from './Footer';
-
-import {connect} from 'react-redux';
-import {bindActionCreators} from 'redux';
-import * as employeeActions from './actions/employeeActions';
-
-import {getApiCall, postApiCall} from './Api.js';
-
-var birthdayFormData = require( './progressForm/datas/inputDatas.js');
-var releaseFormData = require( './progressForm/datas/ReleaseFormData.js');
 
 require('../sass/styles.global.scss');
 
-export const App = React.createClass ({
-	getInitialState() {
-		return ({
-		  teams: [],
-		  employees: [],
-		  releases: [],
-		  showAddEmployeeForm: false,
-		  showAddReleaseForm: false
-		});
-	},
-	componentWillMount(){
-		this.getTeamData();
-		this.getEmployeeData();
-		this.getReleaseData();
-	},
-	componentDidMount () {
-		$('.ui.modal')
-		.modal({
-		  detachable: false,
-		  closable  : true
-		})
-	},
-	getTeamData() {
-		const successCallback = (value) => {
-		  if (value._embedded) {
-			if (value._embedded.scrumTeams) {
-			  this.setState({teams: value._embedded.scrumTeams});
-			};
-		  };
-		}
-
-		const errorCallBack = (value) => {
-
-		}
-
-		getApiCall('/RewardsWidgetAPI/scrumteams', null, successCallback, errorCallBack);
-
-	},
-	getEmployeeData() {
-		const successCallback = (value) => {
-		  if (value._embedded) {
-			if (value._embedded.employees) {
-			  this.setState({employees: value._embedded.employees});
-			};
-		  };
-		}
-
-		const errorCallBack = (value) => {
-
-		}
-
-		getApiCall('/RewardsWidgetAPI/employees', null, successCallback, errorCallBack);
-	},
-	getReleaseData(){
-		const successCallback = (value) => {
-		  if(value._embedded){
-			if(value._embedded.releases){
-			  this.setState({releases: value._embedded.releases});
-			};
-		  };
-		}
-
-		const errorCallBack = (value) => {
-
-		}
-
-		getApiCall('/RewardsWidgetAPI/releases', null, successCallback, errorCallBack);
-	},
-	createNewEmployee (value) {
-		const successCallback = (value) => {
-		  this.getEmployeeData();
-		  this.onClosed();
-		}
-
-		const errorCallBack = (value) => {
-
-		}
-
-		postApiCall('/RewardsWidgetAPI/employees', value, successCallback, errorCallBack);
-	},
-	createNewRelease (value) {
-		const successCallback = (value) => {
-		  this.getReleaseData();
-		  this.onClosed();
-		}
-
-		const errorCallBack = (value) => {
-
-		}
-
-		postApiCall('/RewardsWidgetAPI/releases', value, successCallback, errorCallBack);
-	},
-	onClosed () {
-		this.setState({showAddEmployeeForm: false, showAddReleaseForm: false})
-	},
-	showAddEmployeeForm () {
-		this.setState({showAddEmployeeForm: true});
-	},
+export default class App extends React.Component {
 	render () {
 		return (
 		  <div className="homePage">
@@ -141,11 +22,11 @@ export const App = React.createClass ({
 				<UpcomingEvents/>
 				<EmployeesOfTheMonth/>
 				<QuoteOfTheDay/>
-				<ShowTeams teams={this.state.teams}/>
-				<BirthdayCard employees={this.state.employees}/>
-				<ShowReleaseCards releases={this.state.releases}/>
+				<ShowTeams/>
+				<BirthdayCard/>
+				<ShowReleaseCards/>
 				<Footer/>
 		  </div>
 		);
 	}
-});
+}
--- reactWidget/src/client/app/index.js
+++ reactWidget/src/client/app/index.js
@@ -1,8 +1,8 @@
-import React from 'react'
-import {render} from 'react-dom'
-import {Provider} from 'react-redux'
-import configureStore from './store/configureStore'
-import {App} from './App'
+import React from 'react';
+import {render} from 'react-dom';
+import {Provider} from 'react-redux';
+import configureStore from './store/configureStore';
+import App from './App';
 
 const store = configureStore();
 
@@ -11,4 +11,4 @@ render(
     <App />
   </Provider>,
   document.getElementById('app')
-)
+);
