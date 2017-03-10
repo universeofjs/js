9e29832b62e445bd423a1d39879c7cc21e53b372
Ankit Jain <t443296@10.37.32.35>
2017-03-09 22:08:39 -0500

    updated header text

--- reactWidget/src/client/app/Header.js
+++ reactWidget/src/client/app/Header.js
@@ -5,7 +5,7 @@ export default class Header extends React.Component {
         return (
             <div className="header">
                 <img src="/img/deluxe.png" alt="Deluxe Rewards"/>
-                <h1>Bulletin Board</h1>
+                <h1>Deluxe Bulletin Board</h1>
 				<hr/>
             </div>
         );
